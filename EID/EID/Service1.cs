d194c3648c10f3d015ca79d5169f15d6e7420c68
Craig Kergald <dev@kergald.net>
2013-04-18 10:13:47 -0700

    Initial Commit
    Version 1.0
    EID is the service
    EventIDInstaller is a installer for installing EID

---
+++ EID/EID/Service1.cs
@@ -0,0 +1,348 @@
+﻿// EventID service
+// Version 1.0
+// Licensed under GPL V3
+// Compile as service.exe and bundle with our installer
+// or compile and use the SC command to install as a service
+// if installing as a service manually make sure all registry entries exist before starting service.
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Xml;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+using Microsoft.Win32;
+using System.Threading;
+using System.Text;
+
+
+
+
+namespace EID
+{
+    public partial class EID : ServiceBase
+    {
+        public EID()
+        {
+            InitializeComponent();
+        }
+        // Global variable keeprunning
+        // volatile so it can be accessed across threads
+        // while true service is running
+        private volatile bool keeprunning;
+
+        protected override void OnStart(string[] args)
+        {
+            //
+            // Startup service
+            // spin off new thread for service to run in
+            //
+            ThreadStart ts = new ThreadStart( this.start_main);
+            Thread m_thread = new Thread(ts);
+            m_thread.Start();
+        }
+
+        protected override void OnStop()
+        {
+            //
+            // Stopping service
+            // setting global variable keeprunning to false.
+            //
+            keeprunning = false;
+        }
+
+        public static string Getdomain()
+        {
+            // returns domain name of this computer
+            var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            return string.Format("{0}", ipProperties.DomainName);
+        }
+        public static string Gethost()
+        {
+            // returns host name of this computer
+            var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            return string.Format("{0}", ipProperties.HostName);
+        }
+        public string[] check_reg(string host, string domain, int interval)
+        {
+            // startup check
+            // read values out of registry
+            // if they do not exist let's create them
+            // with default values
+            string[] returnval = new string[8];
+            const string keyroot = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Squidworks\\EventID";
+            try
+            {
+                string temp = Registry.GetValue(keyroot, "hostname", "").ToString();
+                if (temp != "")
+                {
+                    returnval[0] = temp;
+                }
+                temp = Registry.GetValue(keyroot, "domain", "").ToString();
+                if (temp != "")
+                {
+                    returnval[1] = temp;
+                }
+                temp = Registry.GetValue(keyroot, "interval", "").ToString();
+                if (temp != "")
+                {
+                    returnval[2] = temp;
+                }
+                temp = Registry.GetValue(keyroot, "username", "").ToString();
+                if (temp != "")
+                {
+                    returnval[3] = temp;
+                }
+                temp = Registry.GetValue(keyroot, "password", "").ToString();
+                if (temp != "")
+                {
+                    returnval[4] = temp;
+                }
+                temp = Registry.GetValue(keyroot, "debug", "").ToString();
+                if (temp != "")
+                {
+                    returnval[5] = temp;
+                }
+                temp = Registry.GetValue(keyroot, "debug log path", "").ToString();
+                if (temp != "")
+                {
+                    returnval[6] = temp;
+                }
+                temp = Registry.GetValue(keyroot, "Event ID URL", "").ToString();
+                if (temp != "")
+                {
+                    returnval[7] = temp;
+                }
+            }
+            catch
+            {
+                returnval[0] = host;
+                returnval[1] = domain;
+                returnval[2] = interval.ToString();
+                returnval[3] = "";
+                returnval[4] = "";
+                returnval[5] = "false";
+                returnval[6] = @"c:\temp\eid.txt";
+                returnval[7] = @"";
+            }
+            return returnval;
+        }
+        public void start_main()
+        {
+            string cs = "EventID";
+            bool debugging = false;
+            string debugpath = @"c:\temp\eid.txt";
+            string eidusername = "";
+            string eidpassword = "";
+            string url = @"";
+            if (!EventLog.SourceExists(cs))
+                EventLog.CreateEventSource(cs, "Application"); 
+            string host = Gethost();
+            string domain = "";
+            int interval = 15;   
+            domain = Getdomain();
+            if (domain == "")
+            {
+                domain = "Workgroup";
+            }
+            // 0 = host
+            // 1 = domain
+            // 2 = interval for event scanning
+            // 3 = username override
+            // 4 = password override
+            // 5 = debugging true/false
+            // 6 = debug log path
+            // 7 = url override
+            string[] nfo = check_reg(host, domain, interval);
+            host = nfo[0];
+            domain = nfo[1];
+            interval = int.Parse(nfo[2]);
+            double tempdouble = (double)interval;
+            DateTime last_run = DateTime.Now.AddMinutes(-tempdouble);
+            DateTime nextrun = DateTime.Now.AddMinutes(tempdouble);
+            if (nfo[3] != "") { eidusername = nfo[3]; }
+            if (nfo[4] != "") { eidpassword = nfo[4]; }
+            if (nfo[5].ToUpper() == "TRUE")
+            {
+                debugging = true;
+                if (nfo[6] != "") {debugpath = nfo[6];}
+            }
+            if (nfo[7] != "") { url = nfo[7]; }
+            if (debugging)
+            {
+                update_debug_log("NEXT RUN " + nextrun.ToString(),debugpath);
+                update_debug_log("LAST RUN " + last_run.ToString(), debugpath);
+                update_debug_log("URL " + url, debugpath);
+                update_debug_log("Username " + eidusername, debugpath);
+                update_debug_log("Password " + eidpassword, debugpath);
+            }
+            if (nfo[7] != "") { url = nfo[7]; }
+            keeprunning = true;
+            while (keeprunning)
+            {
+                if (DateTime.Now > nextrun)
+                {
+                    if (debugging) { update_debug_log("Starting scan", debugpath); }
+                    DateTime startscantime = DateTime.Now;
+                    EventLog eventlog = new EventLog();
+                    EventLog[] log = EventLog.GetEventLogs();
+                    List<string> eventid = new List<string>();
+                    List<string> source = new List<string>();
+                    List<string> message = new List<string>();
+                    List<string> datetimes = new List<string>();
+                    List<string> logname = new List<string>();
+                    for (int x = 0; x < log.Length; x++)
+                    {
+                        try
+                        {
+                            eventlog.Log = log[x].LogDisplayName.ToString();
+                            eventlog.MachineName = ".";
+                            var eventList = new List<EventLogEntry>();
+                            foreach (EventLogEntry entry in eventlog.Entries)
+                            {
+                                eventList.Add(entry);
+                            }
+                            eventList.Reverse();
+                            for (int y = 0; y < eventList.Count; y++)
+                            {
+                                DateTime testdate = eventList[y].TimeGenerated;
+                                if (last_run > testdate)
+                                {
+                                    if (debugging) { update_debug_log("BREAK " + last_run.ToString() + " is larger than " + testdate.ToString(), debugpath); }
+                                    break;
+                                }
+                                if ((eventList[y].EntryType != EventLogEntryType.Information) && (eventList[y].EntryType != EventLogEntryType.SuccessAudit))
+                                {
+                                    eventid.Add(eventList[y].EventID.ToString());
+                                    source.Add(eventList[y].Source.ToString());
+                                    message.Add(eventList[y].Message.ToString());
+                                    datetimes.Add(eventList[y].TimeGenerated.ToString());
+                                    logname.Add(log[x].LogDisplayName.ToString());
+                                }
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            if (debugging)
+                            {
+                                update_debug_log("Failed to read event log  # " + x.ToString(),debugpath);
+                                update_debug_log("Execption message " + e.Message.ToString(),debugpath);
+                            }
+                        }
+                    }
+                    eventlog.Dispose();
+                    XmlDocument doc = new XmlDocument();
+                    XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+                    doc.AppendChild(docNode);
+                    XmlNode reportNode = doc.CreateElement("report");
+                    doc.AppendChild(reportNode);
+                    for (int i = 0; i < eventid.Count; i++)
+                    {
+                        XmlNode eventnode = doc.CreateElement("event");
+                        XmlAttribute eid = doc.CreateAttribute("ID");
+                        eid.Value = eventid[i];
+                        XmlAttribute src = doc.CreateAttribute("SOURCE");
+                        src.Value = source[i];
+                        XmlAttribute log1 = doc.CreateAttribute("LOG");
+                        log1.Value = logname[i];
+                        XmlAttribute tme = doc.CreateAttribute("LOGTIME");
+                        tme.Value = datetimes[i];
+                        XmlAttribute msg = doc.CreateAttribute("MESSAGE");
+                        msg.Value = message[i];
+                        XmlAttribute hst = doc.CreateAttribute("HOST");
+                        hst.Value = host;
+                        XmlAttribute dmn = doc.CreateAttribute("DOMAIN");
+                        dmn.Value = domain;
+                        eventnode.Attributes.Append(eid);
+                        eventnode.Attributes.Append(src);
+                        eventnode.Attributes.Append(log1);
+                        eventnode.Attributes.Append(tme);
+                        eventnode.Attributes.Append(hst);
+                        eventnode.Attributes.Append(dmn);
+                        eventnode.Attributes.Append(msg);
+                        reportNode.AppendChild(eventnode);
+                    }
+                    string xmldoc = doc.InnerXml.ToString();
+                    if (debugging)
+                    {
+                        string tempfile = debugpath + ".xml";
+                        doc.Save(tempfile);
+                    }
+                    try
+                    {
+                        const string keyroot = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Squidworkds\\EventID";
+                        WebClient wc1 = new WebClient();
+                        wc1.Credentials = new NetworkCredential(eidusername, eidpassword);
+                        wc1.Headers["Accept"] = "/";
+                        wc1.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                        string url_return = wc1.UploadString(url, xmldoc);
+                        nextrun = DateTime.Now.AddMinutes(tempdouble);
+                        last_run = startscantime;
+                        if (debugging) { update_debug_log("Uploaded XML to " + url, debugpath); update_debug_log("RETURN " + url_return, debugpath); }
+                        if (url_return.Contains("not a client")) { remove_self(); }
+                        if (url_return.Contains("domain="))
+                        {
+                            string[] tempvals = url_return.Split('=');
+                            Registry.SetValue(keyroot, "domain", tempvals[1]);
+                            domain = tempvals[1];
+                        }
+                        if (url_return.Contains("hostname="))
+                        {
+                            string[] tempvals = url_return.Split('=');
+                            Registry.SetValue(keyroot, "hostname", tempvals[1]);
+                            host = tempvals[1];
+                        }
+                        if (url_return.Contains("interval="))
+                        {
+                            string[] tempvals = url_return.Split('=');
+                            Registry.SetValue(keyroot, "interval", tempvals[1]);
+                            interval = int.Parse(tempvals[1]);
+                        }
+                    }
+                    catch
+                    {
+                        if (debugging)
+                        {
+                            update_debug_log("An error occured while uploading to " + url, debugpath);
+                            update_debug_log("Username " + eidusername, debugpath);
+                            update_debug_log("Password " + eidpassword, debugpath);
+                        }
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+        public void update_debug_log(string text, string filename)
+        {
+            try
+            {
+                TextWriter debug = new StreamWriter(filename, true);
+                debug.WriteLine(DateTime.Now.ToString() + "  -  " + text);
+                debug.Close();
+            }
+            catch { }
+        }
+        public void remove_self()
+        {
+            // this routine should remove this service
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "sc.exe";
+            startInfo.Arguments = "delete EventID";
+            process.StartInfo = startInfo;
+            process.Start();
+            startInfo.FileName = "sc.exe";
+            startInfo.Arguments = "stop EventID";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
---
+++ EVENTIDINSTALLER/EVENTIDINSTALLER/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace EVENTIDINSTALLER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_step = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_hostname = new System.Windows.Forms.TextBox();
+            this.tb_domain = new System.Windows.Forms.TextBox();
+            this.tb_interval = new System.Windows.Forms.TextBox();
+            this.tb_url = new System.Windows.Forms.TextBox();
+            this.tb_install_path = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(443, 175);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_step
+            // 
+            this.tb_step.Location = new System.Drawing.Point(22, 225);
+            this.tb_step.Name = "tb_step";
+            this.tb_step.Size = new System.Drawing.Size(100, 20);
+            this.tb_step.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hostname";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Domain";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Interval in Minutes";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "URL of your Event ID Site";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Installation Path";
+            // 
+            // tb_hostname
+            // 
+            this.tb_hostname.Location = new System.Drawing.Point(90, 22);
+            this.tb_hostname.Name = "tb_hostname";
+            this.tb_hostname.Size = new System.Drawing.Size(385, 20);
+            this.tb_hostname.TabIndex = 8;
+            // 
+            // tb_domain
+            // 
+            this.tb_domain.Location = new System.Drawing.Point(90, 48);
+            this.tb_domain.Name = "tb_domain";
+            this.tb_domain.Size = new System.Drawing.Size(385, 20);
+            this.tb_domain.TabIndex = 9;
+            // 
+            // tb_interval
+            // 
+            this.tb_interval.Location = new System.Drawing.Point(128, 74);
+            this.tb_interval.Name = "tb_interval";
+            this.tb_interval.Size = new System.Drawing.Size(347, 20);
+            this.tb_interval.TabIndex = 10;
+            // 
+            // tb_url
+            // 
+            this.tb_url.Location = new System.Drawing.Point(165, 100);
+            this.tb_url.Name = "tb_url";
+            this.tb_url.Size = new System.Drawing.Size(310, 20);
+            this.tb_url.TabIndex = 11;
+            // 
+            // tb_install_path
+            // 
+            this.tb_install_path.Location = new System.Drawing.Point(117, 166);
+            this.tb_install_path.Name = "tb_install_path";
+            this.tb_install_path.Size = new System.Drawing.Size(358, 20);
+            this.tb_install_path.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Username";
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(90, 135);
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(167, 20);
+            this.tb_username.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(274, 138);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Password";
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(333, 135);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(142, 20);
+            this.tb_password.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 249);
+            this.Controls.Add(this.tb_password);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tb_username);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tb_install_path);
+            this.Controls.Add(this.tb_url);
+            this.Controls.Add(this.tb_interval);
+            this.Controls.Add(this.tb_domain);
+            this.Controls.Add(this.tb_hostname);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_step);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "EventID Reporting";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_step;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_hostname;
+        private System.Windows.Forms.TextBox tb_domain;
+        private System.Windows.Forms.TextBox tb_interval;
+        private System.Windows.Forms.TextBox tb_url;
+        private System.Windows.Forms.TextBox tb_install_path;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tb_password;
+    }
+}
+
